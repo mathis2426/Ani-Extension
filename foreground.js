22cbf4351c75dd666bb2a52c61ab3e0510c89532
Mathis GRAMAGE <gmathis13@outlook.fr>
2025-09-10 18:38:18 +0200

    F-004 -Adding assets for code optimisation

--- foreground.js
+++ foreground.js
@@ -1,5 +1,16 @@
 let editorExtensionId = "olggkeglcmmolkpmnpffffcpcpdlonpk";
 
+// Loader paresseux pour le module de filtre
+let _AnimeFilter;
+
+async function useAnimeFilter() {
+  if (!_AnimeFilter) {
+    const mod = await import(chrome.runtime.getURL("assets/AnimeFilter.js"));
+    _AnimeFilter = mod.AnimeFilter;
+  }
+  return _AnimeFilter();
+}
+
 class anime {
   constructor() {
     this.name = "";
@@ -257,6 +268,10 @@ async function sendRequestFindAnime() {
         hasTriggered = true;
 
         console.log("animetitle : ", animeNameFromParent);
+
+        // Appel du filtre juste avant la requête
+        useAnimeFilter().catch(console.error);
+
         fetchAllAnimes(animeNameFromParent);
       }
     });
@@ -343,4 +358,3 @@ async function fetchAllAnimes(animeTitle) {
     return [];
   }
 }
-
